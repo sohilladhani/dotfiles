020ce0e8115365278f14781a3cca0a1a8a8e500e
Sohil Ladhani <supertakumi86@gmail.com>
2019-07-29 00:19:20 +0530

    Make cp point to the dir where this script resides

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 # This script copies the dotfiles under ~/ to the current dotfiles git repo
 
-cp -fv ~/{.vimrc,.tmux.conf,.zshrc,.bashrc,.config/nvim/init.vim,.aliases} .
+full_path="$(readlink -f ${BASH_SOURCE[0]})"
+full_path_dir="$(dirname $full_path)"
+cp -fv ~/{.vimrc,.tmux.conf,.zshrc,.bashrc,.config/nvim/init.vim,.aliases} $full_path_dir
