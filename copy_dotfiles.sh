11116d88b565a1d2d96f61cc680535c2cdf41f44
Sohil Ladhani <supertakumi86@gmail.com>
2019-07-28 23:21:30 +0530

    Included .aliases

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 # This script copies the dotfiles under ~/ to the current dotfiles git repo
 
-cp -fv ~/{.vimrc,.tmux.conf,.zshrc,.bashrc,.config/nvim/init.vim} .
+cp -fv ~/{.vimrc,.tmux.conf,.zshrc,.bashrc,.config/nvim/init.vim,.aliases} .
